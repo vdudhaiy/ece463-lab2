a5fae786c458c05cba3325f9e3afb8de9ba3b6bf
Vatsal Sanjeevkum Dudhaiya <vdudhaiy@ececomp3.ecn.purdue.edu>
2025-09-29 13:58:12 -0400

    initial commit

---
+++ http_server/http_server.c
@@ -0,0 +1,37 @@
+/* The code is subject to Purdue University copyright policies.
+ * Do not share, distribute, or post online.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define LISTEN_QUEUE 50 /* Max outstanding connection requests; listen() param */
+
+#define DBADDR "127.0.0.1"
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        fprintf(stderr, "usage: ./http_server [server port] [DB port]\n");
+        exit(1);
+    }
+
+    
+
+    return 0;
+}
